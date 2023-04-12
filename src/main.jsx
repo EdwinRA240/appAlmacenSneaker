508e20b1f84f542e89afcfefffe495284ef2c46b
Edwin Ramírez Aguilar <edwin.raguilar@roatech.com.mx>
2023-04-11 22:07:15 -0600

    feat: inicio, routeo and signin

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./components/SignIn";
+import NotFound from "./components/NotFound";
+import NavBar from "./components/NavBar";
+import NavBar2 from "./components/NavBar2";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <NavBar2 /> <SignIn />
+            </>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
+        {/* <Route path="/Empleado" element={<Empleado />} /> */}
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
